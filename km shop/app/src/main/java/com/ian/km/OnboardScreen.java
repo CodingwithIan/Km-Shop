a13ed1eb06358bafeb8fc6a418e715af3e31ca57
CodingwithIan <iannyangauu@gmail.com>
2022-12-22 17:18:27 +0300

    Mega Project

---
+++ km shop/app/src/main/java/com/ian/km/OnboardScreen.java
@@ -0,0 +1,13 @@
+package com.ian.km;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class OnboardScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboard_screen);
+    }
+}
---
+++ km shop/app/src/main/java/com/ian/km/PayActivity.java
@@ -0,0 +1,135 @@
+package com.ian.km;
+
+import static com.ian.km.Constants.BUSINESS_SHORT_CODE;
+import static com.ian.km.Constants.CALLBACKURL;
+import static com.ian.km.Constants.PARTYB;
+import static com.ian.km.Constants.PASSKEY;
+import static com.ian.km.Constants.TRANSACTION_TYPE;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.ian.km.paymentmodel.AccessToken;
+import com.ian.km.paymentmodel.STKPush;
+import com.ian.km.services.DarajaApiClient;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import timber.log.Timber;
+
+public class PayActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private DarajaApiClient mApiClient;
+    private ProgressDialog mProgressDialog;
+
+    @BindView(R.id.etAmount)
+    EditText mAmount;
+    @BindView(R.id.etPhone)EditText mPhone;
+    @BindView(R.id.btnPay)
+    Button mPay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay);
+        ButterKnife.bind(this);
+
+        mProgressDialog = new ProgressDialog(this);
+        mApiClient = new DarajaApiClient();
+        mApiClient.setIsDebug(true); //Set True to enable logging, false to disable.
+
+        mPay.setOnClickListener(this);
+
+        getAccessToken();
+
+    }
+
+    public void getAccessToken() {
+        mApiClient.setGetAccessToken(true);
+        mApiClient.mpesaService().getAccessToken().enqueue(new Callback<AccessToken>() {
+            @Override
+            public void onResponse(@NonNull Call<AccessToken> call, @NonNull Response<AccessToken> response) {
+
+                if (response.isSuccessful()) {
+                    mApiClient.setAuthToken(response.body().accessToken);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<AccessToken> call, @NonNull Throwable t) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view== mPay){
+            String phone_number = mPhone.getText().toString();
+            String amount = mAmount.getText().toString();
+            performSTKPush(phone_number,amount);
+        }
+    }
+
+
+    public void performSTKPush(String phone_number,String amount) {
+        mProgressDialog.setMessage("Processing your request");
+        mProgressDialog.setTitle("Please Wait...");
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.show();
+        String timestamp = Utility.getTimestamp();
+        STKPush stkPush = new STKPush(
+                BUSINESS_SHORT_CODE,
+                Utility.getPassword(BUSINESS_SHORT_CODE, PASSKEY, timestamp),
+                timestamp,
+                TRANSACTION_TYPE,
+                String.valueOf(amount),
+                Utility.sanitizePhoneNumber(phone_number),
+                PARTYB,
+                Utility.sanitizePhoneNumber(phone_number),
+                CALLBACKURL,
+                "Km shopping", //Account reference
+                "Mpesa integration"  //Transaction description
+        );
+
+        mApiClient.setGetAccessToken(false);
+
+        //Sending the data to the Mpesa API, remember to remove the logging when in production.
+        mApiClient.mpesaService().sendPush(stkPush).enqueue(new Callback<STKPush>() {
+            @Override
+            public void onResponse(@NonNull Call<STKPush> call, @NonNull Response<STKPush> response) {
+                mProgressDialog.dismiss();
+                try {
+                    if (response.isSuccessful()) {
+                        Timber.d("post submitted to API. %s", response.body());
+                    } else {
+                        Timber.e("Response %s", response.errorBody().string());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<STKPush> call, @NonNull Throwable t) {
+                mProgressDialog.dismiss();
+                Timber.e(t);
+            }
+        });
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
